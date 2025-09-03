1ec0d0780162f6253cd5e7aaa57368a6c6ec579c
putputri1 <putrinarilat@gmail.com>
2025-09-04 00:01:44 +0900

    hapus file yang tidak diperlukan

--- mobileput/bin/mobileput.dart
+++
@@ -1,5 +0,0 @@
-import 'package:mobileput/mobileput.dart' as mobileput;
-
-void main(List<String> arguments) {
-  print('Hello world: ${mobileput.calculate()}!');
-}
--- mobileput/test/mobileput_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:mobileput/mobileput.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
